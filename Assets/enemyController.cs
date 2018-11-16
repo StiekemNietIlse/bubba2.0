91bcab1c74844335acbbca87a1b595af88368f12
ilse nijhuis <ilsenijhuis99@gmail.com>
2018-11-16 10:16:48 +0100

    enemy + right size

---
+++ Assets/enemyController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyController : MonoBehaviour {
+
+    public float Speed = 1f;
+    private Rigidbody2D _body;
+    private int Startpoint;
+    public int Endpoint;
+
+    float fMinX = 0.54f;
+    float fMaxX = 1.88f;
+    int Direction = -1;
+    float fEnemyX = 0;
+    float fEnemyY = 0;
+
+    
+
+    // Use this for initialization
+    void Start () {
+        _body = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("hit");
+
+        }
+        if (other.gameObject.tag == "Floor")
+        {
+            Debug.Log("ground!");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        fEnemyX = _body.position.x;
+        fEnemyY = _body.position.y;
+
+        switch (Direction)
+        {
+            case -1:
+                // Moving Left
+                if (fEnemyX > fMinX)
+                {
+                    fEnemyX -= 1.0f;
+                }
+                else
+                {
+                    // Hit left boundary, change direction
+                    Direction = 1;
+                }
+                break;
+
+            case 1:
+                // Moving Right
+                if (fEnemyX < fMaxX)
+                {
+                    fEnemyX += 1.0f;
+                }
+                else
+                {
+                    // Hit right boundary, change direction
+                    Direction = -1;
+                }
+                break;
+        }
+
+        _body.transform.localPosition = new Vector3(fEnemyX, fEnemyY, 0.0f);
+
+    }
+
+    public void FixedUpdate()
+    {
+       
+
+       
+
+
+    }
+}
---
+++ Assets/movement2.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement2 : MonoBehaviour {
+
+    public float Speed = 5f;
+    public float JumpHeight = 0;
+    //public float maxJumpHeight = 3f;
+    public Joystick joystick;
+
+    private Rigidbody2D _body;
+    private Vector2 _inputs = Vector2.zero;
+    private bool spaceKeyDown = false;
+
+    [SerializeField]
+    private bool isGrounded = false;
+    
+    void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {
+            isGrounded = true;
+        }
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {
+            isGrounded = false;
+        }
+    }
+
+    void Update()
+    {
+        //_inputs.x = 0;
+        //_inputs.y = 0;
+        //if (joystick.Horizontal > 0f)
+        //{
+        //    _inputs.x = Speed;
+        //}
+        //if (joystick.Horizontal < 0f)
+        //{
+        //    _inputs.x = -Speed;
+        //}  
+    }
+
+   public void JumpData()
+    {
+        if (isGrounded == true)
+        {
+            Debug.Log("Jump");
+            _body.AddForce(Vector2.up * JumpHeight);
+        }
+    }
+
+    //public void JumpRelease()
+    //{
+    //    JumpHeight = 0f;
+    //}
+    
+    public void FixedUpdate()
+    { 
+        _body.velocity = new Vector2(joystick.Horizontal * Speed, _body.velocity.y);
+
+        //_body.velocity = _inputs;
+        //if (JumpHeight > 0) {
+        //  //  transform.Translate(Vector2.up * JumpHeight * Time.smoothDeltaTime);
+
+        //}
+        // _body.AddForce(Vector3.up * JumpHeight);
+
+        //GetComponent<Rigidbody2D>().AddForce(Vector2.up* 1 , ForceMode2D.Impulse);
+        //transform.Translate(0, JumpHeight , 0 );
+    }
+}
